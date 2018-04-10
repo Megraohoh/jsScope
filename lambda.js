b4579647aa9c89d3c21f358f7e744031af2623de
Meghan Debity <mdebity@gmail.com>
2018-04-10 13:38:37 -0500

    corrected lambda file

--- lambda.js
+++ lambda.js
@@ -1,13 +1,10 @@
 const llamaNamer = function () {
     const possibleNames = ["Larry", "Leon", "Leona", "Les", "Laura", "Lemony", "Lars", "Lekisha"]
     const randomizer = Math.floor(Math.random() * 7)
-    let name = possibleNames[randomizer]
-    let suffix = " the Llama"
-    
+    const suffix = " the Llama"
+    const name = possibleNames[randomizer]
+
     return name + suffix
 }
-// const namer = function () {
-//     nameMaker = llamaNamer()
-// }
-
-// console.log(nameMaker())
+nameMaker = llamaNamer()
+console.log(nameMaker)
